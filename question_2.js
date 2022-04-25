42976c8e36e1912980a09c065502ec956de5da4d
tuan-nguyen-egany <tuan.nguyen@egany.com>
2022-04-25 20:56:36 +0700

    fix: wrong anser of question 2

--- question_2.js
+++ question_2.js
@@ -6,15 +6,48 @@ với a = [-1, 2, -2, -3, 5] thì threenumbers(a) = 18. giải thích: f(1, 2, 5
 [Đầu ra] giá trị lớn nhất của hàm f(i, j, k) tìm được. 
 */
 
+// Comment line dưới để tắt tính năng debug
+// console.debug = function() {};
+
 function threenumbers(a){
-	const sortDescending = a.sort((a, b ) => b - a)
+	let ai = a[0]
+	let aj = a[1]
+	let ak = a[2]
 
-	const i = sortDescending[2]
-	const j = sortDescending[1]
-	const k = sortDescending[0]
+	let aiIndex = 0
+	let ajIndex = 0
+	let akIndex = 0
 
-	return i + (2 * j) + (3 * k)
+	let maxSizeOfSearchAk = a.length - 1
+	for (let k = 0; k <= maxSizeOfSearchAk; k ++) {
+		if (a[k] >= ak) {
+			akIndex = k
+			ak = a[k]
+		}
+	}
+
+	let maxSizeOfSearchAj = maxSizeOfSearchAk - 1
+	for (let j = 0; j <= maxSizeOfSearchAj; j ++) {
+		if (a[j] >= aj && a[j] < ak) {
+			ajIndex = j
+			aj = a[j]
+		}
+	}
+
+	let maxSizeOfSearchAi = maxSizeOfSearchAj - 1
+	for (let i = 0; i <= maxSizeOfSearchAi; i ++) {
+		if (a[i] >= ai && a[i] < aj) {
+			aiIndex = i
+			ai = a[i]
+		}
+	}
+
+	console.debug("ai: ", ai)
+	console.debug("aj: ", aj)
+	console.debug("ak: ", ak)
+
+	return ai + (2 * aj) + (3 * ak)
 }
 
-const A = [-1, 2, -2, -3, 5, 22, 1, 4]
+const A = [-1, 2, -2, -3, 5, 6, -3, 1, 4, 7]
 console.log(threenumbers(A))
